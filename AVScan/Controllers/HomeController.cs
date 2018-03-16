4ccff3590b581a079e2adeb7872304838b5b34f0
Amitsingh Rajput <amitsinghr@gmail.com>
2018-03-16 18:30:14 +0530

    Collated code to home controller

--- AVScan/Controllers/HomeController.cs
+++ AVScan/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
 ﻿using AvScanLibrary;
 using AvScanLibrary.Models;
 using AvScanLibrary.Services;
+using AVScanLibrary;
 using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 
 namespace AVScan.Controllers
 {
     public class HomeController : Controller
     {
+        static string _filePath = null;
+        private ScanOutputs output;
+
         public ActionResult Index()
         {
             return View();
@@ -40,6 +46,66 @@ namespace AVScan.Controllers
             }
 
             return RedirectToAction("Details", new { id = scanid });
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                string _FileName = string.Empty;
+                if (file.ContentLength > 0)
+                {
+                    _FileName = Path.GetFileName(file.FileName).Replace(" ", "");
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                    Vault.FilePath = _path;
+                    Vault.FileName = file.FileName;
+                }
+                ViewBag.Message = string.Format("{0} File Uploaded Successfully!!", _FileName);
+                return View("choosescan", _filePath);
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+        public ActionResult ChooseScan()
+        {
+            if (string.IsNullOrEmpty(Vault.FileName))
+                return View("uploadfile");
+            else
+                return View();
+        }
+
+        [HttpGet]
+        public ActionResult Scan(string servicename)
+        {
+            if (string.IsNullOrEmpty(servicename))
+            {
+                return View("uploadfile");
+            }
+            else
+            {
+                ScannerService service = new ScannerService();
+                output = new ScanOutputs();
+                output.ScanResults = service.Run(servicename, Vault.FilePath);
+                Vault.FileName = null;
+                Vault.FilePath = null;
+            }
+
+            return View(output);
         }
     }
 }
